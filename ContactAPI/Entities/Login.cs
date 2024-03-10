8d761d6ee143315a06f0a66f777beb8d46eeb5a7
moyesan <moyesan@gmail.com>
2024-03-11 04:08:00 +0530

    API changes for Authorization & Logger
    Authorization
    Logger

---
+++ ContactAPI/Entities/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactAPI.Entities
+{
+    public class Login
+    {
+        public string? UserName
+        {
+            get;
+            set;
+        }
+        public string? Password
+        {
+            get;
+            set;
+        }
+    }
+
+    public class JWTTokenResponse
+    {
+        public string? Token
+        {
+            get;
+            set;
+        }
+    }
+}
